6760dbac32940fc483368661afaf7e20929ab585
Alexander Zintler <524585+rbtrrnss@users.noreply.github.com>
2021-02-28 12:59:34 +0100

    Update code.js
    added (hopefully) somepwer saving functionality:
    after the BLE HID control bit is done, NRF.setConnectionInterval(200)
    added an indicatior on the play/pause function to indicate a low(er) battery level

--- code.js
+++ code.js
@@ -1,15 +1,22 @@
 var controls = require("ble_hid_controls");
 NRF.setServices(undefined, { hid : controls.report });
 
+//number of colume measurements
 var measurements = 30;
+//interval between rotation measurements
 var interval = 200;
+//global variable that needs to be created once
 var counts = 0;
 
 setWatch(function(e) {
+  //change to a fast NRF connection interval
+  NRF.setConnectionInterval(30);
   var len = e.time - e.lastTime;
   if (len < 0.33) {
     //playpause
     digitalPulse(LED2,1,10);
+    //low battery warning
+    if (Puck.getBatteryPercentage() < 95) { digitalPulse(LED,1,250); }
     controls.playpause();
     //print("playpause");
   } else {
@@ -35,9 +42,12 @@ setWatch(function(e) {
       else if (az-prev > -9 && az-prev < 9 ) { //print("no change"); 
                                               }
       else {}
-      //safe az as previous reading
+      //save az as previous reading
       prev = az;
+      Puck.magOff();
       }, start*interval);
     }
   }
+  //revert to slow (power saving) interval
+  setTimeout(() => { NRF.setConnectionInterval(200); }, measurements*interval);
 }, BTN, {edge:"falling",repeat:true,debounce:50});
